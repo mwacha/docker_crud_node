b12dc78e71d5646869f5e425850d501b090266ca
Marcelo Wacha <mwacha@gmail.com>
2021-10-27 21:09:14 -0300

    beta version

--- node/app.js
+++ node/app.js
@@ -6,7 +6,7 @@ const app = express();
 const urlencodeParser=bodyParser.urlencoded({extended:false});
 
 const connection = mysql.createConnection({ 
-    host: 'db',
+    host: 'database',
     user: 'root',
     password: 'root',
     port: 3306,
@@ -22,27 +22,28 @@ app.set('view engine','handlebars');
 // Static files
 app.use("/css", express.static("css"));
 app.use("/js", express.static("js"));
-app.use("/img", express.static("img"));
+
 
 app.get("/", function(req,res) {
      res.render('index');
    })
 // Routes and templates
-app.get("/:id?", function(req,res) {
+
+app.get("/select:id?", function(req,res) {
     if (!req.params.id) {
-        connection.query("select * from person order by person_name", function(err, results, fields){
-          res.render("index", {data:results});
+        connection.query("select * from person order by person_name", function(err, results){
+          res.render("select", {data:results});
         });
     } else {
-        connection.query("select * from person where id = ?", [req.params.id], function(err,results, fields){
-          res.render("index", {data:results});
+        connection.query("select * from person where id = ?", [req.params.id], function(results){
+          res.render("select", {data:results});
         });
     }  
   });
 
-  /*app.get("/", function(req,res) {
+  app.get("/", function(req,res) {
     res.render("index");
-});*/
+});
 app.get("/create", function(req,res) {
     res.render("create");
 });
@@ -53,14 +54,19 @@ app.post("/createControllerForm", urlencodeParser, function (req,res) {
 });
 
 app.get("/update/:id", urlencodeParser, function(req,res) {
-    con.query("select * from person where id =?",[req.params.id], function(err, results, fields){
+  connection.query("select * from person where id =?",[req.params.id], function(err, results){
       res.render("update", {data:results});
     });  
   });
   
-  app.put("/update/updateControllerForm", urlencodeParser, function(req,res) {
-    con.query("update people set name = ? where id = ?", [req.body.name, req.body.id]);
+  app.post("/update/updateControllerForm", urlencodeParser, function(req,res) {
+    connection.query("update person set person_name = ? where id = ?", [req.body.name, req.body.id]);
     res.render("updateControllerForm");
+  });
+
+  app.get("/remove/:id", function(req,res) {
+    connection.query("delete from person where id =?",[req.params.id]);
+    res.render("remove");
   });
 
 //start server
--- script/init.sql
+++ script/init.sql
@@ -12,5 +12,3 @@ SET character_set_client = utf8;
 SET character_set_connection = utf8;
 SET character_set_results = utf8;
 SET collation_connection = utf8_general_ci;
-
-INSERT INTO person (person_name) VALUES ('Marcelo Wacha');
